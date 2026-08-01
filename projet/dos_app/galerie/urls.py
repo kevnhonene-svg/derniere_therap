133d828b2b0b0026160099838ae4f2caf0c1f02b
joseph <ngalaj33@gmail.com.com>
2026-08-01 18:23:27 +0100

    jjjfj

--- projet/dos_app/galerie/urls.py
+++ projet/dos_app/galerie/urls.py
@@ -5,6 +5,7 @@ from dos_app.galerie import views
 urlpatterns = [
     path('albums/', views.albums_public, name='galerie_albums_public'),
     path('photos/', views.photos_public, name='galerie_photos_public'),
+    path('photos/<int:photo_id>/jpeg/', views.photo_jpeg, name='galerie_photo_jpeg'),
     path('photos/<int:photo_id>/telecharger/', views.telecharger_photo, name='galerie_telecharger_photo'),
     path('admin/albums/', views.albums_admin, name='galerie_albums_admin'),
     path('admin/albums/<int:album_id>/', views.album_admin_detail, name='galerie_album_admin_detail'),
